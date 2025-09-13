d8fdc41b11b0bce3a73d73732c450aaeacd7d172
Kevin Requelme <kevinrequelme@gmail.com>
2025-09-13 17:15:21 -0300

    Se implemento la Clase Automatizacion de forma mas completa, ademas se corrigieron algunos test

--- automatizaciones.py
+++ automatizaciones.py
@@ -1,26 +1,63 @@
+from datetime import datetime
+
 class Automatizaciones:
     def __init__(self, dispositivos):
         self.dispositivos = dispositivos
+        self.hora_activacion_modo_noche = 23
 
     def consultar_automatizaciones(self):
-        modo = True
-        for dispositivo in self.dispositivos:
-            if dispositivo["tipo"] == 1 and dispositivo["estado"] == False:
-                modo = False
-                break
+        luces_equipomusica = [dispositivo["estado"] for dispositivo in self.dispositivos if dispositivo["tipo"] in (2, 3)]
+        modo_fiesta = all(luces_equipomusica) if luces_equipomusica else False
 
-        estado_modo = "On" if modo else "Off"
+        camaras = [dispositivo["estado"] for dispositivo in self.dispositivos if dispositivo["tipo"] == 1]
+        modo_noche = any(camaras) if camaras else False
 
-        return f"Estado de Automatizaciones: - Modo: {estado_modo}"
+        return modo_fiesta, modo_noche
+
+    def mostrar_estado(self):
+        modo_fiesta, modo_noche = self.consultar_automatizaciones()
+        estado_fiesta = "On" if modo_fiesta else "Off"
+        estado_noche = "On" if modo_noche else "Off"
+        return f"Estado de Automatizaciones: - Modo Fiesta: {estado_fiesta} / - Modo Noche: {estado_noche}"
+
     
-    def activar_modo(self):
+    def activar_modo_fiesta(self):
         for dispositivo in self.dispositivos:
+            if dispositivo["tipo"] in [2, 3] and dispositivo["estado"] == False:
+                dispositivo["estado"] = True
+        return "Modo Fiesta activado: Equipo de Musica y Luces encendidas."
+    
+    def apagar_modo_fiesta(self):
+        for dispositivo in self.dispositivos:
+            if dispositivo["tipo"] in [2, 3] and dispositivo["estado"] == True:
+                dispositivo["estado"] = False
+        return "Modo fiesta desactivado: Equipo de Musica y Luces apagadas."
+    
+    def activar_modo_noche(self):
+        for dispositivo in self.dispositivos:
+            if dispositivo["tipo"] in [2,3] and dispositivo["estado"] == True:
+                dispositivo["estado"] = False
             if dispositivo["tipo"] == 1 and dispositivo["estado"] == False:
                 dispositivo["estado"] = True
-        return "Modo activado"
-    
-    def apagar_modo(self):
+        return "Modo noche activado: Camaras encendidas, equipo de musica y luces apagadas."
+    def apagar_modo_noche(self):
         for dispositivo in self.dispositivos:
             if dispositivo["tipo"] == 1 and dispositivo["estado"] == True:
                 dispositivo["estado"] = False
-        return "Modo apagado"
+        return "Modo noche desactivado: Camaras apagadas."
+    
+    def configurar_hora_modo_noche(self, nueva_hora: int):
+        if nueva_hora < 0 or nueva_hora > 23:
+            return False
+        self.hora_activacion_modo_noche = nueva_hora
+        return True
+
+    def verificar_hora_modo_noche(self):
+        hora_actual = datetime.now().hour
+        modo_fiesta, modo_noche = self.consultar_automatizaciones()
+
+        if modo_fiesta or modo_noche:
+            return False
+        if hora_actual != self.hora_activacion_modo_noche:
+            return False
+        return True
--- tests/test_automatizaciones.py
+++ tests/test_automatizaciones.py
@@ -4,27 +4,36 @@ from automatizaciones import Automatizaciones
 class TestAutomatizaciones(unittest.TestCase):
 
     def setUp(self):
-        self.dispositivos = [{"tipo": 1, "estado": True}]
+        self.dispositivos = [{"tipo": 1, "estado": True}, {"tipo" : 2, "estado": True}, {"tipo": 3, "estado": True}]
         self.auto = Automatizaciones(self.dispositivos)
+        self.estado_fiesta = Automatizaciones(self.estado_fiesta)
 
     def test_consultar_automatizaciones_activas(self):
-        resultado = self.auto.consultar_automatizaciones()
-        self.assertEqual(resultado, "Estado de Automatizaciones: - Modo: On")
-    
+        resultado = self.auto.mostrar_estado()
+        self.assertEqual(resultado, "Estado de Automatizaciones: - Modo Fiesta: On / - Modo Noche: On")
+
     def test_consultar_automatizaciones_apagadas(self):
-        self.dispositivos[0]["estado"] = False
-        resultado = self.auto.consultar_automatizaciones()
-        self.assertEqual(resultado, "Estado de Automatizaciones: - Modo: Off")
+        for dispositivo in self.dispositivos:
+            dispositivo["estado"] = False
+        resultado = self.auto.mostrar_estado()
+        self.assertEqual(resultado, "Estado de Automatizaciones: - Modo Fiesta: Off / - Modo Noche: Off")
+
+    
     
     def test_activar_modo(self):
-        mensaje = self.auto.activar_modo()
-        self.assertEqual(mensaje, "Modo activado")
-        self.assertTrue(self.dispositivos[0]["estado"])
+        mensaje = self.auto.activar_modo_fiesta()
+        self.assertEqual(mensaje, "Modo Fiesta activado: Equipo de Musica y Luces encendidas.")
+        self.assertTrue(self.dispositivos[1]["estado"])
+        self.assertTrue(self.dispositivos[2]["estado"])
 
     def test_apagar_modo(self):
-        mensaje = self.auto.apagar_modo()
-        self.assertEqual(mensaje, "Modo apagado")
-        self.assertFalse(self.dispositivos[0]["estado"])
+        mensaje = self.auto.apagar_modo_fiesta()
+        self.assertEqual(mensaje, "Modo fiesta desactivado: Equipo de Musica y Luces apagadas.")
+        self.assertFalse(self.dispositivos[1]["estado"])
+        self.assertFalse(self.dispositivos[2]["estado"])
 
+    def test_configurar_hora_valida(self):
+        self.assertTrue(self.auto.configurar_hora_modo_noche(22))
+        self.assertEqual(self.auto.hora_activacion_modo_noche, 22)
 if __name__ == "__main__":
     unittest.main()
