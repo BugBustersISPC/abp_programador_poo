9db71ee207115fdee9a23dfd2d284f8f7dfaa9c0
Kevin Requelme <kevinrequelme@gmail.com>
2025-09-15 17:19:33 -0300

    Finalizacion de la Clase Automatizacion

--- tests/test_automatizaciones.py
+++ tests/test_automatizaciones.py
@@ -1,57 +1,54 @@
 import unittest
 from automatizaciones import Automatizaciones
+from dispositivo import Dispositivo
 
 class TestAutomatizaciones(unittest.TestCase):
 
     def setUp(self):
-        self.dispositivos = [{"tipo": 1, "estado": True}, {"tipo" : 2, "estado": True}, {"tipo": 3, "estado": True}]
-        self.auto = Automatizaciones(self.dispositivos)
+        self.camara = Dispositivo("Camara Entrada", Dispositivo.TIPO_CAMARA, True)
+        self.luz = Dispositivo("Luz Living", Dispositivo.TIPO_LUZ, True)
+        self.musica = Dispositivo("Equipo Musica Living", Dispositivo.TIPO_MUSICA, True)
+        self.auto = Automatizaciones([self.camara, self.luz, self.musica])
 
     def test_consultar_automatizaciones_activas(self):
         resultado = self.auto.mostrar_estado()
         self.assertEqual(resultado, "Estado de Automatizaciones: - Modo Fiesta: On / - Modo Noche: On")
 
     def test_consultar_automatizaciones_apagadas(self):
-        dispositivos = self.auto.get_dispositivos()
-        for dispositivo in dispositivos:
-            dispositivo["estado"] = False
+        self.camara.apagar()
+        self.luz.apagar()
+        self.musica.apagar()
+
         resultado = self.auto.mostrar_estado()
         self.assertEqual(resultado, "Estado de Automatizaciones: - Modo Fiesta: Off / - Modo Noche: Off")
 
     def test_fiestaoff_nocheon(self):
-        dispositivos = self.auto.get_dispositivos()
-        dispositivos[1]["estado"] = False
-        dispositivos[2]["estado"] = False
+        self.luz.apagar()
+        self.musica.apagar()
         
         resultado = self.auto.mostrar_estado()
         self.assertEqual(resultado, "Estado de Automatizaciones: - Modo Fiesta: Off / - Modo Noche: On")
     
     def test_fiestaon_nocheoff(self):
-        dispositivos = self.auto.get_dispositivos()
-        dispositivos[0]["estado"] = False
+        self.camara.apagar()
 
         resultado = self.auto.mostrar_estado()
         self.assertEqual(resultado, "Estado de Automatizaciones: - Modo Fiesta: On / - Modo Noche: Off")
 
     def test_activar_modo_fiesta(self):
-        dispositivos = self.auto.get_dispositivos()
-        dispositivos[1]["estado"] = False
-        dispositivos[2]["estado"] = False
+        self.luz.apagar()
+        self.musica.apagar()
 
         mensaje = self.auto.activar_modo_fiesta()
         self.assertEqual(mensaje, "Modo Fiesta activado: Equipo de Musica y Luces encendidas.")
-        self.assertTrue(self.dispositivos[1]["estado"])
-        self.assertTrue(self.dispositivos[2]["estado"])
+        self.assertTrue(self.luz.estado)
+        self.assertTrue(self.musica.estado)
 
     def test_apagar_modo_fiesta(self):
-        dispositivos = self.auto.get_dispositivos()
-        dispositivos[1]["estado"] = True
-        dispositivos[2]["estado"] = True
-
         mensaje = self.auto.apagar_modo_fiesta()
         self.assertEqual(mensaje, "Modo fiesta desactivado: Equipo de Musica y Luces apagadas.")
-        self.assertFalse(self.dispositivos[1]["estado"])
-        self.assertFalse(self.dispositivos[2]["estado"])
+        self.assertFalse(self.luz.estado)
+        self.assertFalse(self.musica.estado)
 
     def test_configurar_hora_valida(self):
         self.auto.set_hora_activacion_modo_noche(22)
