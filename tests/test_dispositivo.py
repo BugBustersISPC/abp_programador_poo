24b61c2f1cae89ad9b348c5e9c7dcab3bbc3749b
Fausto Fili <faustosantinofili@gmail.com>
2025-09-16 01:17:37 -0300

    cambio unittest por pytest

--- tests/test_dispositivo.py
+++ tests/test_dispositivo.py
@@ -1,45 +1,40 @@
-import unittest
-from dispositivo import Dispositivo 
+import pytest
+from dispositivo import Dispositivo
 
-class TestDispositivo(unittest.TestCase):
-    
-    def test_creacion_dispositivo_valido(self):
-        dispositivo = Dispositivo(nombre="Luz Living", tipo=Dispositivo.TIPO_LUZ, estado=False)
-        self.assertEqual(dispositivo.nombre, "Luz Living")
-        self.assertEqual(dispositivo.tipo, 2)
-        self.assertEqual(dispositivo.estado, False)
-        
-    def test_creacion_falla_por_nombre_vacio(self):
-        with self.assertRaises(ValueError):
-            Dispositivo(nombre="", tipo=Dispositivo.TIPO_LUZ, estado=True)
-            
-    def test_creacion_falla_por_tipo_invalido(self):
-        with self.assertRaises(ValueError):
-            Dispositivo(nombre="Dispositivo Raro", tipo=99, estado=True)
-            
-    def test_cambiar_estado(self):
-        dispositivo = Dispositivo(nombre="Cámara Jardín", tipo=Dispositivo.TIPO_CAMARA, estado=False)
-        self.assertFalse(dispositivo.estado)
-        
-        # Probamos encender
-        dispositivo.encender()
-        self.assertTrue(dispositivo.estado)
-        
-        # Probamos apagar
-        dispositivo.apagar()
-        self.assertFalse(dispositivo.estado)
-        
-    def test_falla_al_cambiar_estado_con_valor_no_booleano(self):
-        dispositivo = Dispositivo(nombre="Parlante", tipo=Dispositivo.TIPO_MUSICA, estado=True)
-        with self.assertRaises(ValueError):
-            dispositivo.estado = "apagado"
+def test_creacion_dispositivo_valido():
+    dispositivo = Dispositivo(nombre="Luz Living", tipo=Dispositivo.TIPO_LUZ, estado=False)
+    assert dispositivo.nombre == "Luz Living"
+    assert dispositivo.tipo == 2
+    assert dispositivo.estado is False
 
-    def test_representacion_string(self):
-        disp_on = Dispositivo("Luz RGB", Dispositivo.TIPO_LUZ, True)
-        disp_off = Dispositivo("Alarma", Dispositivo.TIPO_CAMARA, False)
-        
-        self.assertEqual(str(disp_on), "Luz RGB (Tipo: Luz) - Estado: On")
-        self.assertEqual(str(disp_off), "Alarma (Tipo: Cámara) - Estado: Off")
+def test_creacion_falla_por_nombre_vacio():
+    with pytest.raises(ValueError):
+        Dispositivo(nombre="", tipo=Dispositivo.TIPO_LUZ, estado=True)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_creacion_falla_por_tipo_invalido():
+    with pytest.raises(ValueError):
+        Dispositivo(nombre="Dispositivo Raro", tipo=99, estado=True)
+
+def test_cambiar_estado():
+    dispositivo = Dispositivo(nombre="Cámara Jardín", tipo=Dispositivo.TIPO_CAMARA, estado=False)
+    assert dispositivo.estado is False
+
+    #probamos encender
+    dispositivo.encender()
+    assert dispositivo.estado is True
+
+    #probamos apagar
+    dispositivo.apagar()
+    assert dispositivo.estado is False
+
+def test_falla_al_cambiar_estado_con_valor_no_booleano():
+    dispositivo = Dispositivo(nombre="Parlante", tipo=Dispositivo.TIPO_MUSICA, estado=True)
+    with pytest.raises(ValueError):
+        dispositivo.estado = "apagado"
+
+def test_representacion_string():
+    disp_on = Dispositivo("Luz RGB", Dispositivo.TIPO_LUZ, True)
+    disp_off = Dispositivo("Alarma", Dispositivo.TIPO_CAMARA, False)
+
+    assert str(disp_on) == "Luz RGB (Tipo: Luz) - Estado: On"
+    assert str(disp_off) == "Alarma (Tipo: Cámara) - Estado: Off"
